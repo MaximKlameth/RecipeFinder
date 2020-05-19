1378fe249392010e9637a648778015054b288276
Kevingies <giesen.kevin@yahoo.de>
2020-05-19 18:08:40 +0200

    DB 3.Commit KG

--- app/src/main/java/net/lehre_online/android/recipefinder1/RecipeMemo.java
+++ app/src/main/java/net/lehre_online/android/recipefinder1/RecipeMemo.java
@@ -1,45 +1,77 @@
 package net.lehre_online.android.recipefinder1;
 
+import java.sql.Blob;
+
 public class RecipeMemo {
 
-    private String rezept;
-    private String beschreibung;
-    private long id;
+    private long rez_id;
+    private String rez_name;
+    private String rez_beschreibung;
+    private String rez_dauer;
+    private Blob rez_bild;
+    private long rez_kalorien;
 
 
-    public RecipeMemo(String rezept, String beschreibung, long id) {
-        this.rezept = rezept;
-        this.beschreibung = beschreibung;
-        this.id = id;
+
+
+    public RecipeMemo(long rez_id,String rez_name, String rez_beschreibung, String rez_dauer, Blob rez_bild, long rez_kalorien) {
+        this.rez_name = rez_name;
+        this.rez_beschreibung = rez_beschreibung;
+        this.rez_id = rez_id;
+        this.rez_dauer = rez_dauer;
+        this.rez_bild= rez_bild;
+        this.rez_kalorien = rez_kalorien;
+    }
+
+    public long getRez_id() {
+        return rez_id;
+    }
+
+    public void setRez_id(long rez_id) {
+        this.rez_id = rez_id;
     }
 
 
-    public String getRezept() {
-        return rezept;
+    public String getRez_name() {
+        return rez_name;
     }
 
-    public void setRezept(String rezept) {
-        this.rezept = rezept;
-    }
-
-
-    public String getBeschreibung() {
-        return beschreibung;
-    }
-
-    public void setBeschreibung(String beschreibung) {
-        this.beschreibung = beschreibung;
-
+    public void setRez_name(String rez_name) {
+        this.rez_name = rez_name;
     }
 
 
-    public long getId() {
-        return id;
+    public String getRez_beschreibung() {
+        return rez_beschreibung;
     }
 
-    public void setId(long id) {
-        this.id = id;
+    public void setRez_beschreibung(String rez_beschreibung) {
+        this.rez_beschreibung = rez_beschreibung;
     }
+
+    public String getRez_dauer(){
+        return rez_dauer;
+    }
+
+    public void setRez_dauer(){
+        this.rez_dauer = rez_dauer;
+    }
+
+    public Blob getRez_bild(){
+        return rez_bild;
+    }
+    public void setRez_bild(){
+        this.rez_bild = rez_bild;
+    }
+
+    public long getRez_kalorien(){
+        return rez_kalorien;
+    }
+
+    public void setRez_kalorien(){
+        this.rez_kalorien = rez_kalorien;
+    }
+
 
 
 }
--- app/src/main/java/net/lehre_online/android/recipefinder1/RecipeMemoDbHelper.java
+++ app/src/main/java/net/lehre_online/android/recipefinder1/RecipeMemoDbHelper.java
@@ -5,24 +5,59 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-    public class RecipeMemoDbHelper extends SQLiteOpenHelper{
+import java.sql.Blob;
+
+public class RecipeMemoDbHelper extends SQLiteOpenHelper{
 
         private static final String LOG_TAG = RecipeMemoDbHelper.class.getSimpleName();
 
 
+        public static final String DB_NAME = "rezept_list.db";
+        public static final int DB_VERSION = 1;
+
+        public static final String TABLE_REZEPT_LIST = "rezept_list";
+
+        public static final String COLUMN_ID = "rez_id";
+        public static final String COLUMN_NAME = "rez_name";
+        public static final String COLUMN_BESCHREIBUNG = "rez_beschreibung";
+        public static final String COLUMN_DAUER = "rez_dauer";
+        public static final String COLUMN_BILD = "rez_bild";
+        public static final String COLUMN_KALORIEN = "rez_kalorien";
+
+
+        public static final String SQL_CREATE_REZEPT =
+                "CREATE TABLE " + TABLE_REZEPT_LIST +
+                        "(" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        COLUMN_NAME + " TEXT NOT NULL, " +
+                        COLUMN_BESCHREIBUNG + " TEXT NOT NULL, " +
+                        COLUMN_DAUER + " TEXT NOT NULL, " +
+                        COLUMN_BILD + " BLOB NOT NULL, " +
+                        COLUMN_KALORIEN + " INTEGER NOT NULL + );";
+
+
         public RecipeMemoDbHelper(Context context) {
-            super(context, "PLATZHALTER_DATENBANKNAME", null, 1);
+            //super(context, "PLATZHALTER_DATENBANKNAME", null, 1);
+            super(context, DB_NAME, null, DB_VERSION);
             Log.d(LOG_TAG, "DbHelper hat die Datenbank: " + getDatabaseName() + " erzeugt.");
         }
 
+        // Die onCreate-Methode wird nur aufgerufen, falls die Datenbank noch nicht existiert
         @Override
         public void onCreate(SQLiteDatabase db) {
+            try {
+                Log.d(LOG_TAG, "Die Tabelle wird mit SQL-Befehl: " + SQL_CREATE_REZEPT + " angelegt.");
+                db.execSQL(SQL_CREATE_REZEPT);
 
+            }
+            catch (Exception ex) {
+                Log.e(LOG_TAG, "Fehler beim Anlegen der Tabelle: " + ex.getMessage());
+            }
         }
 
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
         }
+
+
     }
 
