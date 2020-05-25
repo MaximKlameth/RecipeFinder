cd2c386bd37fa31150bc78a2f1694142d9e06f17
MaximKlameth <maxim.klameth@gmail.com>
2020-05-25 14:13:17 +0200

    Add Ingredient Funktion eingefügt.

--- app/src/main/java/net/lehre_online/android/recipefinder1/RecipeSearchActivity.java
+++ app/src/main/java/net/lehre_online/android/recipefinder1/RecipeSearchActivity.java
@@ -3,10 +3,14 @@ package net.lehre_online.android.recipefinder1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.text.Layout;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class RecipeSearchActivity extends AppCompatActivity {
 
@@ -14,6 +18,8 @@ public class RecipeSearchActivity extends AppCompatActivity {
     static final String TAG         = "RecipeSearchActivity";
 
     private Button btnSearch;
+    private Button btnAddIngredient;
+    private Button btnAddIngredient2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +37,27 @@ public class RecipeSearchActivity extends AppCompatActivity {
             }
         });
 
+        btnAddIngredient = findViewById(R.id.Button_AddIngredient);
+        btnAddIngredient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                onClickAddIngredient();
+            }
+        });
+
+        btnAddIngredient2 = findViewById(R.id.Button_AddIngredient2);
+        btnAddIngredient2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                onClickAddIngredient2();
+            }
+        });
+
         if(DBG)Log.d(TAG,MNAME +"entering...");
+
+
 
     }
 
@@ -42,6 +68,34 @@ public class RecipeSearchActivity extends AppCompatActivity {
 
         Intent intent2 = new Intent( RecipeSearchActivity.this, ResultListActivity.class );
         startActivity(intent2);
+
+        if( DBG ) Log.d( TAG, MNAME + "...exiting" );
+    }
+
+    private void onClickAddIngredient() {
+
+        final String MNAME = "onClickAddIngredient";
+        if( DBG ) Log.v( TAG, MNAME + "entering..." );
+
+        Button button_AddIngredient = (Button) findViewById(R.id.Button_AddIngredient);
+        button_AddIngredient.setVisibility(View.INVISIBLE);
+
+        LinearLayout layout3_recipe_search = (LinearLayout) findViewById(R.id.layout3_recipe_search);
+        layout3_recipe_search.setVisibility(View.VISIBLE);
+
+        if( DBG ) Log.d( TAG, MNAME + "...exiting" );
+    }
+
+    private void onClickAddIngredient2() {
+
+        final String MNAME = "onClickAddIngredient2";
+        if( DBG ) Log.v( TAG, MNAME + "entering..." );
+
+        Button button_AddIngredient2 = (Button) findViewById(R.id.Button_AddIngredient2);
+        button_AddIngredient2.setVisibility(View.INVISIBLE);
+
+        EditText editText_SearchField5 = (EditText) findViewById(R.id.EditText_SearchField5);
+        editText_SearchField5.setVisibility(View.VISIBLE);
 
         if( DBG ) Log.d( TAG, MNAME + "...exiting" );
     }
